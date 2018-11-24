5277341d5716193e2d20bbdaaf19714267913352
Roy Wei <roywjy@gmail.com>
2018-11-25 00:02:02 +1100

    Set up mutation component

--- sick-fits/frontend/components/CreateItem.js
+++ sick-fits/frontend/components/CreateItem.js
@@ -39,50 +39,56 @@ class CreateItem extends Component {
   }
   render() {
     return (
-      <Form onSubmit={(e) => {
-        e.preventDefault();
-      }}>
-        <fieldset>
-          <label htmlFor="title">
-            Title
-            <input
-              type="text"
-              id="title"
-              name="title"
-              placeholder="Title"
-              required
-              value={this.state.title}
-              onChange={this.handleChange}
-          />
-          </label>
-
-          <label htmlFor="price">
-            Price
-            <input
-              type="number"
-              id="price"
-              name="price"
-              placeholder="Price"
-              required
-              value={this.state.price}
-              onChange={this.handleChange}
-          />
-          </label>
-
-          <label htmlFor="description">
-            Description
-            <textarea
-              id="description"
-              name="description"
-              placeholder="Enter A Description"
-              required
-              value={this.state.description}
-              onChange={this.handleChange}
-          />
-          </label>
-          <button type="submit">Submit</button>
-        </fieldset>
-      </Form>
+      <Mutation mutation={CREATE_ITEM_MUTATION} variables ={this.state}>
+        {(createItem, { loading, error }) => (
+          <Form
+            onSubmit={(e) => {
+              e.preventDefault();
+            }}
+          >
+            <fieldset>
+              <label htmlFor="title">
+                Title
+                <input
+                  type="text"
+                  id="title"
+                  name="title"
+                  placeholder="Title"
+                  required
+                  value={this.state.title}
+                  onChange={this.handleChange}
+              />
+              </label>
+    
+              <label htmlFor="price">
+                Price
+                <input
+                  type="number"
+                  id="price"
+                  name="price"
+                  placeholder="Price"
+                  required
+                  value={this.state.price}
+                  onChange={this.handleChange}
+              />
+              </label>
+    
+              <label htmlFor="description">
+                Description
+                <textarea
+                  id="description"
+                  name="description"
+                  placeholder="Enter A Description"
+                  required
+                  value={this.state.description}
+                  onChange={this.handleChange}
+              />
+              </label>
+              <button type="submit">Submit</button>
+            </fieldset>
+          </Form>
+        )}
+      </Mutation>
     )
   }
 }
