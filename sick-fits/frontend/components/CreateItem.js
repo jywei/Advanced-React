4dbc94723fb980b6a205852dc848967c392a32b4
Roy Wei <roywjy@gmail.com>
2018-11-24 23:06:46 +1100

    Create title tag and price tag

--- sick-fits/frontend/components/CreateItem.js
+++ sick-fits/frontend/components/CreateItem.js
@@ -12,13 +12,39 @@ class CreateItem extends Component {
     largeImage: '',
     price: 0,
   };
+  handleChange = e => {
+    const { name, type, value } = e.target;
+    const val = type == 'number' ? parseFloat(value) : value;
+    this.setState({ [name]: val });
+  }
   render() {
     return (
       <Form>
         <fieldset>
           <label htmlFor="title">
             Title
-            <input type="text" id="title" name="title" placeholder="Title" required />
+            <input
+              type="text"
+              id="title"
+              name="title"
+              placeholder="Title"
+              required
+              value={this.state.title}
+              onChange={this.handleChange}
+          />
+          </label>
+
+          <label htmlFor="price">
+            Price
+            <input
+              type="number"
+              id="price"
+              name="price"
+              placeholder="Price"
+              required
+              value={this.state.price}
+              onChange={this.handleChange}
+          />
           </label>
         </fieldset>
       </Form>
