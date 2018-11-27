fa8c37ea7b21964c2118d15d8bed8a0df12e125f
Roy Wei <roywjy@gmail.com>
2018-11-27 22:14:34 +1100

    Add updateItem function

--- sick-fits/backend/src/resolvers/Mutation.js
+++ sick-fits/backend/src/resolvers/Mutation.js
@@ -10,7 +10,25 @@ const Mutations = {
     }, info);
 
     return item;
-  }
+  },
+
+  async updateItem(parent, args, ctx, info) {
+    // first take a copy of the updates
+    const updates = { ...args };
+    // remove the ID from the updates
+    delete updates.id;
+    // run update method
+    return ctx.db.mutation.updateItem(
+      {
+        data: updates,
+        where: {
+          id: args.id,
+        },
+      },
+      // info is the actual query from the client side
+      info
+    );
+  },
 };
 
 module.exports = Mutations;
