2e323041a9c2499894a1a3dd68fd80d42a4a0554
Roy Wei <roywjy@gmail.com>
2018-12-18 20:45:36 +1100

    Setup sending email functions

--- sick-fits/backend/src/resolvers/Mutation.js
+++ sick-fits/backend/src/resolvers/Mutation.js
@@ -2,6 +2,7 @@ const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 const { randomBytes } = require('crypto');
 const { promisify } = require('util');
+const { transport, makeANiceEmail } = require('../mail');
 
 const Mutations = {
   async createItem(parent, args, ctx, info) {
@@ -113,9 +114,17 @@ const Mutations = {
       where: { email },
       data: { resetToken, resetTokenExpiry },
     });
-    console.log(res);
-    return { message: 'Thanks!' };
     // 3. Email them that reset token
+    const mailRes = await transport.sendMail({
+      from: 'roywjy@gmail.com',
+      to: user.email,
+      subject: 'Your password reset token!',
+      html: makeANiceEmail(`Your Password Reset Token is here!
+      \n\n
+      <a href="${process.env.FRONTEND_URL}/reset?resetToken=${resetToken}">Click Here to Reset</a>`),
+    });
+    // 4. return the message
+    return { message: 'Thanks!' };
   },
 
   async resetPassword(parent, args, ctx, info) {
@@ -155,7 +164,7 @@ const Mutations = {
     // 8. return the new user
     return updatedUser;
   },
-  
+
 };
 
 module.exports = Mutations;
