ddcdcd34f4e616b9a3b8f6275451d24eaeffaef1
Roy Wei <roywjy@gmail.com>
2019-02-17 11:27:36 +1100

    Add CartCount to Nav

--- sick-fits/frontend/components/Nav.js
+++ sick-fits/frontend/components/Nav.js
@@ -1,6 +1,7 @@
 import Link from 'next/link';
 import NavStyles from './styles/NavStyles';
 import User from './User';
+import CartCount from './CartCount';
 import Signout from './Signout';
 import { Mutation } from 'react-apollo';
 import { TOGGLE_CART_MUTATION } from './Cart';
@@ -26,7 +27,10 @@ const Nav = () => (
             <Signout />
             <Mutation mutation={TOGGLE_CART_MUTATION}>
               {(toggleCart) => (
-                <button onClick={toggleCart}>My Cart</button>
+                <button onClick={toggleCart}>
+                  My Cart
+                  <CartCount count={me.cart.reduce((tally, cartItem) => tally + cartItem.quantity, 0)}></CartCount>
+                </button>
               )}
             </Mutation>
           </>
