380e20c989a0b2004abf0afdfe89972192954021
Roy Wei <roywjy@gmail.com>
2019-02-25 23:32:18 +1100

    Add onChange function in AutoComplete component

--- sick-fits/frontend/components/Search.js
+++ sick-fits/frontend/components/Search.js
@@ -6,12 +6,57 @@ import gql from 'graphql-tag';
 import debounce from 'lodash.debounce';
 import { DropDown, DropDownItem, SearchStyles } from './styles/DropDown';
 
+const SEARCH_ITEMS_QUERY = gql`
+  query SEARCH_ITEMS_QUERY($searchTerm: String!) {
+    items(
+      where: { 
+        OR: [
+          { title_contains: $searchTerm },
+          { description_contains: $searchTerm }
+        ] 
+      }
+    ) {
+      id
+      image
+      title
+    }
+  }
+`;
+
 class AutoComplete extends React.Component {
+  state = {
+    items: [],
+    loading: false,
+  };
+  onChange = debounce(async (e, client) => {
+    console.log('Searching...')
+    // turn loading on
+    this.setState({ loading: true });
+    // Manually query apollo client
+    const res = await client.query({
+      query: SEARCH_ITEMS_QUERY,
+      variables: { searchTerm: e.target.value }
+    });
+    this.setState({
+      items: res.data.items,
+      loading: false,
+    });
+  }, 350);
   render() {
     return(
       <SearchStyles>
         <div>
-          <input type="search" />
+          <ApolloConsumer>
+            {(client) => (
+              <input
+                type="search"
+                onChange={(e) => {
+                  e.persist();
+                  this.onChange(e, client);
+                }}
+              />
+            )}
+          </ApolloConsumer>
           <DropDown>
             <p>Item will go here</p>
           </DropDown>
