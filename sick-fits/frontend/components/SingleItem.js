ece417a5761e2b0f10b7358fdf466f129de68a6d
Roy Wei <roywjy@gmail.com>
2019-06-29 18:15:07 +1000

    Add tests for SingleItem as snapshot

--- sick-fits/frontend/components/SingleItem.js
+++ sick-fits/frontend/components/SingleItem.js
@@ -46,7 +46,7 @@ class SingleItem extends Component {
       >
         {({error, loading, data}) => {
           if (error) return <Error error={error} />;
-          if (loading) return <p>loading...</p>;
+          if (loading) return <p>Loading...</p>;
           if (!data.item) return <p>No Item Found for {this.props.id} </p>
           const item = data.item;
           return(
@@ -68,3 +68,4 @@ class SingleItem extends Component {
 }
 
 export default  SingleItem;
+export { SINGLE_ITEM_QUERY };
