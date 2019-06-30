33e862e3aec281015b2ec62dd647374d1c0693f2
Roy Wei <roywjy@gmail.com>
2019-06-30 18:31:38 +1000

    Add data-test="form" for testing in component

--- sick-fits/frontend/components/RequestReset.js
+++ sick-fits/frontend/components/RequestReset.js
@@ -28,6 +28,7 @@ class RequestReset extends Component {
         {(reset, { error, loading, called }) => (
           <Form
             method="post"
+            data-test="form"
             onSubmit={async e => {
               e.preventDefault();
               await reset();
